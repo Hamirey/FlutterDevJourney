d282c2a3bd133c2181110e573a5ca359bdd0df33
Yuloco <49050558+Yuloco@users.noreply.github.com>
2025-11-16 00:39:41 +0100

    Day 1: First Dart Program

---
+++ Day1/hello.dart
@@ -0,0 +1,16 @@
+// hello.dart
+void main() {
+  print ("hello, @OpenSou75524613! Welcome to Dart");
+
+// Variables
+String name = "OpenSou75524613";
+int age = 25;
+double height = 1.75;
+bool isStudent = true;
+
+//dispaly in console
+print ("Name: $name");
+print ("Age: $age");
+print ("Height: $height m");
+print ("Student: $isStudent");
+}
